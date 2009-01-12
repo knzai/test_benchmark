2ddae41647f1243d0da616131f6147baba8d9da7
Tim Connor <timocratic@gmail.com>
2009-01-12 15:28:06 -0800

    finished porting over to mediator, only outputting once

--- lib/test_benchmark.rb
+++ lib/test_benchmark.rb
@@ -3,26 +3,6 @@ require 'test/unit/testresult'
 require 'test/unit/testcase'
 require 'test/unit/ui/console/testrunner'
 
-# EXPERIMENTAL
-# Show a report of the time each test takes to run
-class Test::Unit::TestSuite
-
-  @@test_benchmarks = {}
-
-  # Runs the tests and/or suites contained in this
-  # TestSuite.
-  def run(result, &progress_block)
-    yield(STARTED, name)
-    @tests.each do |test|
-      start_single_test = Time.now
-      test.run(result, &progress_block)
-      @@test_benchmarks[test.name] = Time.now - start_single_test
-    end
-    yield(FINISHED, name)
-  end
-
-end
-
 module Test
 module Unit
 module UI
@@ -39,35 +19,36 @@ class TestRunner
   
   alias started_old started
   def started(result)
-    started_old
+    started_old(result)
+    @benchmark_times = {}
   end
   
   alias finished_old finished
   def finished(elapsed_time)
-    finished_old
-    @@test_benchmarks = Test::Unit::TestSuite.send(:class_variable_get, '@@test_benchmarks')
-    @@test_benchmarks.keys.sort{|a, b| @@test_benchmarks[a] <=> @@test_benchmarks[b]}.each do |key|
-      value = @@test_benchmarks[key]
+    finished_old(elapsed_time)
+    @benchmark_times.sort{|a, b| a[1] <=> b[1]}.each do |element|
+      value = element[1]
+      key = element[0]
       puts(("%0.3f" % value) + " #{key}") if /^test_/.match(key)
     end
   end
   
   alias test_started_old test_started
   def test_started(name)
-    test_started_old
+    test_started_old(name)
+    @benchmark_times[name] = Time.now
   end
   
   alias test_finished_old test_finished
   def test_finished(name)
-    test_finished
+    test_finished_old(name)
+    @benchmark_times[name] = Time.now - @benchmark_times[name]
   end
   
   def test_suite_started(name)
-    #puts name
   end
   
   def test_suite_finished(name)
-    #puts Test::Unit::TestSuite.send :class_variable_get, '@@test_benchmarks'
   end
 end
 
