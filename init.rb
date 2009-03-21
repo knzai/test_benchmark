2b410d6b34af3bac379f30afa0a1a53ad765b531
Marek de Heus <marek@deheus.net>
2009-03-22 06:21:25 +0800

    Gets out of the way if run via autotest
    Signed-off-by: Tim Connor <timocratic@gmail.com>

--- init.rb
+++ init.rb
@@ -1,1 +1,5 @@
-require File.dirname(__FILE__) + "/lib/test_benchmark" if ENV['RAILS_ENV'] == "test"
+while (pid ||= $$).to_i > 0
+  pid, *process = `ps -p #{pid} -o ppid -o args`.strip.split("\n").last.split
+  @autotest ||= process.join =~ /autotest/i 
+end 
+require File.dirname(__FILE__) + "/lib/test_benchmark" if ENV['RAILS_ENV'] == "test" && !@autotest
