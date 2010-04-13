84b8765f663c64e5550abf1d450a02372300514b
Tim Connor <timocratic@gmail.com>
2010-04-13 16:11:54 -0700

    skip textmate too

--- init.rb
+++ init.rb
@@ -1,7 +1,7 @@
 if ENV['RAILS_ENV'] == 'test' && !%w{false none}.include?(ENV['BENCHMARK'])
   while (pid ||= $$).to_i > 0
     pid, *process = `ps -p #{pid} -o ppid -o args`.strip.split("\n").last.split
-    @autotest ||= process.join =~ /autotest/i 
+    @autotest ||= process.join =~ /autotest|textmate/i 
   end 
 
   require File.dirname(__FILE__) + "/lib/test_benchmark" unless @autotest
